0bef6105fd5fb4041bd8940f9407fdad8dd10b1f
Martin Jablečník <martin.jablecnik@email.cz>
2018-05-05 18:42:01 +0200

    init

---
+++ Game.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+func _ready():
+	show_sprite()
+	
+func show_sprite():
+	var image = Image.new()
+	image.load("res://images/icon.png")
+	var sprite = Sprite.new() 
+	sprite.position = Vector2(0,0)
+	sprite.texture = ImageTexture.new()
+	sprite.texture.create_from_image(image)
+	add_child(sprite) 
+
